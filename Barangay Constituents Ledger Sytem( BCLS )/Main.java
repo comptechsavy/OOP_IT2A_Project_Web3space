42c519530e28fb3e5d1a51a8a20469419ccd6066
Reyjoy Peñanueva Sabino <reyjoy.sabino@dorsu.edu.ph>
2025-12-27 16:12:07 +0800

    Add files via upload

---
+++ Barangay Constituents Ledger Sytem( BCLS )/Main.java
@@ -0,0 +1,18 @@
+
+import view.LoginWindow;
+import database.DatabaseHelper;
+import javax.swing.SwingUtilities;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Initializing BCLS Security...");
+
+        
+        DatabaseHelper.createNewTable(); 
+
+ 
+        SwingUtilities.invokeLater(() -> {
+            new LoginWindow(); 
+        });
+    }
+}
