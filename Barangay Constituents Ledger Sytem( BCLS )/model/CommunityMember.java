1c3ba16e17ed92ba3abfc4340739454b1b29bc6c
Reyjoy Peñanueva Sabino <reyjoy.sabino@dorsu.edu.ph>
2025-12-27 16:10:43 +0800

    Delete Barangay Constituents Ledger Sytem( BCLS ) directory

--- Barangay Constituents Ledger Sytem( BCLS )/model/CommunityMember.java
+++
@@ -1,7 +0,0 @@
-package model;
-
-public abstract class CommunityMember extends Person {
-    public CommunityMember(int id, String name, String contactNumber) {
-        super(id, name, contactNumber);
-    }
-}
